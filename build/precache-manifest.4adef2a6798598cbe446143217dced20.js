fd113841f2627c04f333e6150ccca2bb674e0b6e
pritibpatil <patilpritib15@gmail.com>
2023-01-19 13:52:32 +0530

    first commit

---
+++ build/precache-manifest.4adef2a6798598cbe446143217dced20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecac7e59e1226c35d28a6a2975036131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95dbd06706394f70d01e",
+    "url": "/static/css/main.8bd137a1.chunk.css"
+  },
+  {
+    "revision": "ae07b8ffe8e55ea5df91",
+    "url": "/static/js/2.606b4617.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.606b4617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95dbd06706394f70d01e",
+    "url": "/static/js/main.45baf529.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.45baf529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
